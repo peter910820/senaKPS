88751b57d9368d408b4771f1728eed9fab579c8b
peter910820 <peter910820@gmail.com>
2024-06-28 03:56:48 +0800

    modify the layout of chang key block

--- setting.py
+++ setting.py
@@ -1,8 +1,12 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QSizePolicy, QPushButton, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import (
+    QWidget, QMessageBox, QScrollArea, 
+    QVBoxLayout, QHBoxLayout, QSizePolicy, QPushButton, 
+    QColorDialog, QLabel
+)
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, Qt
 from PyQt5.QtGui import QPixmap, QPainter, QColor
-import sys, json
+import sys
 from pynput import keyboard
 import threading
 
@@ -12,44 +16,119 @@ class SenaKpsSetting(QtWidgets.QWidget):
         super().__init__()
         #mainwindow settings
         self.setObjectName("senaKPS_Setting")
-        self.setWindowTitle('senaKPS-Setting')
-        self.resize(355, 180)
-        self.setFixedSize(355, 180)
-        self.key_table = None
+        self.setWindowTitle('senaKPS Setting')
+        self.resize(335, 300)
+        self.setFixedSize(335, 300)
         self.table_index = 0
         self.ui()
 
     def ui(self):
         vbox = QtWidgets.QWidget(self)
-        vbox.setGeometry(0, 0, 355, 180)
+        vbox.setGeometry(0, 0, 335, 300)
         self.v_layout = QtWidgets.QVBoxLayout(vbox)
         new_key = QPushButton('create new key', self)
         new_key.clicked.connect(self.new_key_click)
+        
+        self.create_key_area()
+
+        hbox = QWidget(self)
+        h_layout = QtWidgets.QHBoxLayout(hbox)
+        h_layout.setContentsMargins(0, 0, 0, 0)
+        backgroundColor = QPushButton('backgroundColor', self)
+        backgroundColor.clicked.connect(lambda: self.color_click(False))
+        self.backgroundColor_color = QLabel(self)
+        self.backgroundColor_color.setStyleSheet('background-color: white')
+        mainColor = QPushButton('mainColor', self)
+        mainColor.clicked.connect(lambda: self.color_click(True))
+        self.mainColor_color = QLabel(self)
+        self.mainColor_color.setStyleSheet('background-color: white')
+        backgroundColor.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.backgroundColor_color.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        mainColor.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.mainColor_color.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        h_layout.addWidget(backgroundColor, 4)
+        h_layout.addWidget(self.backgroundColor_color, 1)
+        h_layout.addWidget(mainColor, 4)
+        h_layout.addWidget(self.mainColor_color, 1)
+
+        opacity = QtWidgets.QDoubleSpinBox(self)
+        opacity.setDecimals(1)
+        opacity.setRange(0, 1)
+        opacity.setSingleStep(0.1)
+        opacity.setValue(1.0)
+
+        ok_key = QPushButton('OK', self)
+        ok_key.clicked.connect(self.ok_key_click)
+
         self.v_layout.addWidget(new_key, alignment=Qt.AlignTop)
+        self.v_layout.addWidget(self.scroll_area)
+        self.v_layout.addWidget(hbox)
+        self.v_layout.addWidget(opacity)
+        self.v_layout.addWidget(ok_key)
+
+    def create_key_area(self):
+        self.key_table = QWidget(self)
+        self.key_table_layout = QtWidgets.QVBoxLayout(self.key_table)
+        self.key_table_layout.setAlignment(Qt.AlignTop)
+
+        key_block = QWidget(self)
+        key_block_layout = QtWidgets.QHBoxLayout(key_block)
+        key_block_layout.setContentsMargins(0, 0, 0, 0)
+        key_block_symbol = QLabel(self)
+        key_block_symbol.setText('Symbol')
+        key_block_key = QLabel(self)
+        key_block_key.setText('Key')
+        key_block_remove = QLabel(self)
+        key_block_remove.setText('Remove')
+        key_block_symbol.setAlignment(Qt.AlignCenter)
+        key_block_key.setAlignment(Qt.AlignCenter)
+        key_block_remove.setAlignment(Qt.AlignCenter)
+        key_block_layout.addWidget(key_block_symbol, alignment=Qt.AlignTop)
+        key_block_layout.addWidget(key_block_key, alignment=Qt.AlignTop)
+        key_block_layout.addWidget(key_block_remove, alignment=Qt.AlignTop)
+        key_block.setFixedHeight(20)
+
+        self.scroll_area = QScrollArea()
+        self.key_table_layout.addWidget(key_block)
+        self.scroll_area.setWidget(self.key_table)
+        self.scroll_area.setWidgetResizable(True)
 
     def new_key_click(self):
         self.key_set = KeySet()
         self.key_set.accept_data.connect(self.accept_data)
         self.key_set.show()
+
+    def ok_key_click(self):
+        pass
+
+    def color_click(self, judge):
+        color = QColorDialog().getColor()
+        print(color.name())
+        if judge:
+            self.mainColor_color.setStyleSheet(f'background-color: {color.name()}')
+        else:
+            self.backgroundColor_color.setStyleSheet(f'background-color: {color.name()}')
+
         
     @pyqtSlot(str)
     def accept_data(self, key):
-        if self.key_table:
-            rows_end = self.key_table.rowCount()
-            self.key_table.insertRow(rows_end)
-        else:
-            self.key_table = QTableWidget(1, 3)
-            self.key_table.setHorizontalHeaderLabels(['symbol', 'key', 'remove'])
+        key_block = QWidget(self)
+        key_block_layout = QtWidgets.QHBoxLayout(key_block)
+        key_block_layout.setContentsMargins(0, 0, 0, 0)
+        key_block_symbol = QLabel(self)
+        key_block_symbol.setText(key)
+        key_block_key = QLabel(self)
+        key_block_key.setText(key)
+        key_block_remove = QPushButton('remove')
+        key_block_symbol.setAlignment(Qt.AlignCenter)
+        key_block_key.setAlignment(Qt.AlignCenter)
+        key_block_layout.addWidget(key_block_symbol)
+        key_block_layout.addWidget(key_block_key)
+        key_block_layout.addWidget(key_block_remove)
+        key_block.setFixedHeight(20)
 
-        symbol = QTableWidgetItem(key)
-        key = QTableWidgetItem(key)
-        remove = QPushButton('remove')
-        remove.clicked.connect(self.remove_button_click)
-        self.key_table.setItem(self.table_index, 0, symbol)
-        self.key_table.setItem(self.table_index, 1, key)
-        self.key_table.setCellWidget(self.table_index, 2, remove)
-        self.v_layout.addWidget(self.key_table)
-        self.table_index += 1
+        self.key_table_layout.addWidget(key_block)
+        self.scroll_area.setWidget(self.key_table)
 
     def remove_button_click(self):
         print('哈哈我還沒做拉')
